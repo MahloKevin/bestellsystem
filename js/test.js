4942dbeb0cb1c220d62fa8b1d58092a873d68fee
MahloKevin <kevin.seitz@mahlo.com>
2024-04-25 14:34:17 +0200

    Beginning

---
+++ js/test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Cards() {
+    return <h1>DAS ist ein Test</h1>;
+}
+
+ReactDOM.render(<Cards />, document.getElementById("Test"));
