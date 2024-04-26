0445a6e649f80b4073b113f1519ab981fa9009ba
MahloKevin <kevin.seitz@mahlo.com>
2024-04-26 08:29:49 +0200

    added react (still don't know why it doesn't work) 2024_04_26

--- js/test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-function Cards() {
-    return <h1>DAS ist ein Test</h1>;
-}
-
-ReactDOM.render(<Cards />, document.getElementById("Test"));
