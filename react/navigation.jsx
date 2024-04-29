66ca7759e00c273685aceda3dfd8f4721224fa7b
MahloKevin <kevin.seitz@mahlo.com>
2024-04-29 07:39:47 +0200

    2024_04_29

--- react/navigation.jsx
+++ react/navigation.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 
 function NavigationBar() {
     // TODO: Actually implement a navigation bar
-    return <h1>Hello from React!</h1>;
+    return <h1>Hello from Test!</h1>;
 }
 
 const domNode = document.getElementById('navigation');
